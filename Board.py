01dcc76b1c7b0bbb094d41d66ce29c44c018a0e7
beth <beth.gooding@hotmail.com>
2021-03-09 21:50:33 +0000

    Annotated Board.py and Piece.py files

--- Board.py
+++ Board.py
@@ -1,21 +1,21 @@
 class Board:
-    def __init__(self, size):
+    def __init__(self, size):                #Constructor in Python basically, defining the properties of a class
         self.n = size
         self.pieces = set()
 
     def size(self):
-        return self.n
+        return self.n                        #A method which returns the size of a given board
 
-    def admissiblePlacementFor(self, piece):
-        for other in self.pieces:
-            if ((other != piece)
-                and other.attacks(piece)
-                or piece.attacks(other)):
-                    return False
-        return True
+    def admissiblePlacementFor(self, piece): #A method which checks whether the placement of piece violates the conditions of the problem
+        for other in self.pieces:            #For the pieces in the board.pieces set
+            if ((other != piece)             #If the other piece is not our specific piece...
+                and other.attacks(piece)     #...and the other piece can attack or piece...
+                or piece.attacks(other)):    #...or our specific piece could attack the other piece
+                    return False             # the placement of the piece is not admissible
+        return True                          #if we haven't returned False, then the placement is admissible
 
-    def add(self, piece):
-        self.pieces.add(piece)
+    def add(self, piece): 
+        self.pieces.add(piece)              #A method which adds a piece to the board, the piece is stored in the board.pieces set
 
     def remove(self, piece):
-        self.pieces.remove(piece)
+        self.pieces.remove(piece)           #A method which removes a piece from the board, the piece is removed from the board.pieces set
--- Piece.py
+++ Piece.py
@@ -1,13 +1,14 @@
 class Piece:
-    def __init__(self):
+    def __init__(self):                                         #Constructs the piece class with the given properties
         self.board = None
         self.i = None
         self.j = None
 
-    def isOnBoard(self):
+    def isOnBoard(self):                                        #Returns true if self.board does not equal none, i.e the piece is on the board
         return self.board != None
 
-    def placeOn(self, board, i, j):
+    def placeOn(self, board, i, j):                             #Defines a method which sets the position properties of the piece, and then places it on the board
+                                                                #using the board.add method from the board class.
         if (not self.isOnBoard()
             and (0 <= i)
             and (i < board.size())
@@ -18,12 +19,14 @@ class Piece:
                 self.j = j;
                 board.add( self );
 
-    def removeFromBoard(self):
+    def removeFromBoard(self):                                  #Defines a method which checks if the piece is on the board and removes it from both the board.pieces set
+                                                                #and changes the self.board property back to none, so that the isOnBoard method will work
         if (self.isOnBoard()):
             self.board.remove(self)
             self.board = None
 
-    def attacks(self, piece):
+    def attacks(self, piece):                                  #This is an abstract method, the attacks method belongs to all classes of pieces, but not the pieces class itself
+                                                               #as the attack is different for each class which inherits from pieces class.
         raise Exception("An abstract method has been invoked")
 
     def isMindfulOf(self, piece):
@@ -33,13 +36,15 @@ class Piece:
                 and self.board == piece.board
                 and self != piece)
     
-    def rowIndex(self):
+    def rowIndex(self):                                         #If we remove the piece from the board, we do not reset the i and j coordinates inside the removeFromBoard function
+                                                                #so if we checked the coordinates we may come to the wrong conclusion. This makes sure it is returned as something
+                                                                #which will flag up an error.
         if (self.isOnBoard()):
             return self.i
         else:
             return self.UNKNOWN
     
-    def colIndex(self):
+    def colIndex(self):                                         #This method does follows the same principal as rowIndex()
         if (self.isOnBoard()):
             return self.j;
         else:
